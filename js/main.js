4626627d17f2e9d6944ee1fe527bfb25583b79cf
Justin Scothorn <scothorn.2@wright.edu>
2015-06-23 14:55:43 -0400

    small fix

--- js/main.js
+++ js/main.js
@@ -10,7 +10,36 @@ define([
 	// LOCAL VARS 
 	var SITE_ROOT = "file:///home/justin/Documents/A.R.T.I.S.T./",
 		templates = TemplateLoader(),
-		pagesObject = [{name: "home", url: "home/home.html", children: [{name: "Risks", url: "home/Risks.html", children: [{name: "Alternatives to IVF", url: "home/Risks/alternatives.html"},{name: "Risks_to_Offspring", url: "home/Risks/Risks_to_Offspring.html", children: [{name: "Overall Risks", url: "home/Risks/Risks_to_Offspring/overall_risks.html"},{name: "Birth Defects", url: "home/Risks/Risks_to_Offspring/birth_defects.html"},{name: "Risks of Multiple Pregnancy", url: "home/Risks/Risks_to_Offspring/multiple_pregnancy.html"}] },{name: "Risks_to_Women", url: "home/Risks/Risks_to_Women.html", children: [{name: "Ovarian Hyperstimulation Syndrome", url: "home/Risks/Risks_to_Women/ohs.html"},{name: "cancer", url: "home/Risks/Risks_to_Women/cancer.html"},{name: "Risks of Pregnancy", url: "home/Risks/Risks_to_Women/pregnancy.html"}] }], },{name: "Social_Legal", url: "home/Social_Legal.html", children: [{name: "Ethical and Religious Considerations in Infertility Treatment", url: "home/Social_Legal/ethics.html"},{name: "Psychological Effects of Infertility Treatment", url: "home/Social_Legal/effects.html"},{name: "Legal Considerations to Offspring", url: "home/Social_Legal/offspring.html"},{name: "Financial", url: "home/Social_Legal/financial.html"},{name: "Reporting Outcomes", url: "home/Social_Legal/outcomes.html"}], },{name: "Technique_of_IVF", url: "home/Technique_of_IVF.html", children: [{name: "Core_Elements_and_Their_Risks", url: "home/Technique_of_IVF/Core_Elements_and_Their_Risks.html", children: [{name: "Medications for IVF Treatment", url: "home/Technique_of_IVF/Core_Elements_and_Their_Risks/medications.html"},{name: "Embryo Transfer", url: "home/Technique_of_IVF/Core_Elements_and_Their_Risks/embryo_transfer.html"},{name: "Transvaginal Oocyte Retrieval", url: "home/Technique_of_IVF/Core_Elements_and_Their_Risks/oocyte_retrieval.html"},{name: "IVF and Embryo Culture", url: "home/Technique_of_IVF/Core_Elements_and_Their_Risks/culture.html"},{name: "Hormonal Support of the Utarine Lining", url: "home/Technique_of_IVF/Core_Elements_and_Their_Risks/uterine_lining.html"}] },{name: "Additional_Elements_and_Their_Risks", url: "home/Technique_of_IVF/Additional_Elements_and_Their_Risks.html", children: [{name: "Intracytoplasmic Sperm Injection", url: "home/Technique_of_IVF/Additional_Elements_and_Their_Risks/injection.html"},{name: "Embryo Disposition", url: "home/Technique_of_IVF/Additional_Elements_and_Their_Risks/embryo_disposition.html"},{name: "Assisted Hatching", url: "home/Technique_of_IVF/Additional_Elements_and_Their_Risks/hatching.html"},{name: "Cryopreserved Embryo Storage", url: "home/Technique_of_IVF/Additional_Elements_and_Their_Risks/storage.html"},{name: "Donated or Research Embryo Fate", url: "home/Technique_of_IVF/Additional_Elements_and_Their_Risks/research.html"}] }] }] }],
+		pagesObject = [{name: "home", url: "home/home.html", children:
+						 [{name: "Risks", url: "home/Risks.html", children: 
+						 	[{name: "Alternatives to IVF", url: "home/Risks/alternatives.html"},
+						 	{name: "Risks_to_Offspring", url: "home/Risks/Risks_to_Offspring.html", children: 
+						 		[{name: "Overall Risks", url: "home/Risks/Risks_to_Offspring/overall_risks.html"},
+						 		{name: "Birth Defects", url: "home/Risks/Risks_to_Offspring/birth_defects.html"},
+						 		{name: "Risks of Multiple Pregnancy", url: "home/Risks/Risks_to_Offspring/multiple_pregnancy.html"}] },
+						 	{name: "Risks_to_Women", url: "home/Risks/Risks_to_Women.html", children: 
+						 		[{name: "Ovarian Hyperstimulation Syndrome", url: "home/Risks/Risks_to_Women/ohs.html"},
+						 		{name: "cancer", url: "home/Risks/Risks_to_Women/cancer.html"},
+						 		{name: "Risks of Pregnancy", url: "home/Risks/Risks_to_Women/pregnancy.html"}] }], },
+					  	 {name: "Social_Legal", url: "home/Social_Legal.html", children: 
+					  	 	[{name: "Ethical and Religious Considerations in Infertility Treatment", url: "home/Social_Legal/ethics.html"},
+					  	 	{name: "Psychological Effects of Infertility Treatment", url: "home/Social_Legal/effects.html"},
+					  	 	{name: "Legal Considerations to Offspring", url: "home/Social_Legal/offspring.html"},
+					  	 	{name: "Financial", url: "home/Social_Legal/financial.html"},
+					  	 	{name: "Reporting Outcomes", url: "home/Social_Legal/outcomes.html"}], },
+					     {name: "Technique_of_IVF", url: "home/Technique_of_IVF.html", children: 
+						 	[{name: "Core_Elements_and_Their_Risks", url: "home/Technique_of_IVF/Core_Elements_and_Their_Risks.html", children: 
+						 		[{name: "Medications for IVF Treatment", url: "home/Technique_of_IVF/Core_Elements_and_Their_Risks/medications.html"},
+						 		{name: "Embryo Transfer", url: "home/Technique_of_IVF/Core_Elements_and_Their_Risks/embryo_transfer.html"},
+						 		{name: "Transvaginal Oocyte Retrieval", url: "home/Technique_of_IVF/Core_Elements_and_Their_Risks/oocyte_retrieval.html"},
+						 		{name: "IVF and Embryo Culture", url: "home/Technique_of_IVF/Core_Elements_and_Their_Risks/culture.html"},
+						 		{name: "Hormonal Support of the Utarine Lining", url: "home/Technique_of_IVF/Core_Elements_and_Their_Risks/uterine_lining.html"}] },
+						 	{name: "Additional_Elements_and_Their_Risks", url: "home/Technique_of_IVF/Additional_Elements_and_Their_Risks.html", children: 
+						 		[{name: "Intracytoplasmic Sperm Injection", url: "home/Technique_of_IVF/Additional_Elements_and_Their_Risks/injection.html"},
+						 		{name: "Embryo Disposition", url: "home/Technique_of_IVF/Additional_Elements_and_Their_Risks/embryo_disposition.html"},
+						 		{name: "Assisted Hatching", url: "home/Technique_of_IVF/Additional_Elements_and_Their_Risks/hatching.html"},
+						 		{name: "Cryopreserved Embryo Storage", url: "home/Technique_of_IVF/Additional_Elements_and_Their_Risks/storage.html"},
+						 		{name: "Donated or Research Embryo Fate", url: "home/Technique_of_IVF/Additional_Elements_and_Their_Risks/research.html"}] }] }] }],
 		that = {
 			list: [],
 			object: {},
@@ -90,6 +119,12 @@ define([
 				mobile: mobile
 			});
 		// ONLY DISPLAY IF NOT MOBILE OR THERE ARE NO MORE CHILDREN
+
+		///////////////////////////////////////////////////////////
+		//
+		// Maybe change this to only show when at a child.....IVF techniques has a page that isnt achild though
+		//
+		///////////////////////////////////////////////////////////
 		if (!mobile || that.list.length===0) {
 			$("#pageContent").html(renderHTML).removeClass("hidden");
 		} else {
