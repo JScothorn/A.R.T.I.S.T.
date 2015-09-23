4b180177184981bb7ba29b1e46c57145ef99f90f
Justin Scothorn <scothorn.2@wright.edu>
2015-09-23 10:51:56 -0400

    minor code clean up

--- js/myCookie.js
+++ js/myCookie.js
@@ -5,9 +5,10 @@
     }
         
     var addCheckmark =function(el) {
+
         el = '#'+el;
         var height = $(el).parent().height();// use el to make sure check marks are the same size as the image it is replacing
-        //$(el).addClass('clicked');
+
         $('img',el).attr('src', 'images/check.png');// swap image
         $('img',el).attr('style', 'height:' + height + 'px');// set proper height
         $('i',el).attr('class', 'fa fa-check fa-5x');// swap icon
@@ -17,19 +18,19 @@
     $(".content").click(function(e){
         e.preventDefault();
 
-       var moduleRef = $(this).attr("id");
-       var list = Cookies.getJSON()||{};
-       list = _.pluck(list,"modules")[0]||[];
+        var moduleRef = $(this).attr("id");
+        var list = Cookies.getJSON()||{};
+        list = _.pluck(list,"modules")[0]||[];
 
-       list.push(moduleRef)
+        list.push(moduleRef)
         Cookies.set('settings', {'modules': list}, {expires: 365});
         addCheckmark(moduleRef)
         
     });
    
     var storeVisited = function(){
+
         var obj = Cookies.getJSON()||{};
-        
 
         _.each(_.pluck(obj,"modules")[0],function(value){
              addCheckmark(value)
